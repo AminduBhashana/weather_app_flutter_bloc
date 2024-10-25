e579d75aec5e72e1e65e2004863bb5cf8d867a8d
AminduBhashana <waamidu@gmail.com>
2024-10-25 21:58:30 +0530

    install and configure packages

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text("Hello World"),
+        ),
+      ),
     );
   }
 }
